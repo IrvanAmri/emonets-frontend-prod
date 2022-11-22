c125de0ae50af7f1dc8a33c0f4682ee0de3d6ca0
IrvanAmri <72341169+IrvanAmri@users.noreply.github.com>
2022-11-22 18:39:56 +0700

    Update axios.js
    set prod url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://emonet-backend2.azurewebsites.net";
 
 export default axios.create({
   baseURL: BASE_URL,

f0e1de10604e40ec5214b0d8fae4b4035e26de0d
IrvanAmri <72341169+IrvanAmri@users.noreply.github.com>
2022-11-26 14:10:33 +0700

    set CI/CD environment & fix register loading

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
+// aktifkan saat fase deploy
 const BASE_URL = "https://emonet-backend2.azurewebsites.net";
+
+//aktifkan saat fase develop
+// const BASE_URL = "http://localhost:8080";
 
 export default axios.create({
   baseURL: BASE_URL,
